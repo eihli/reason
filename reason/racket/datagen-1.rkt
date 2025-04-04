129972537344130a342eb83d9b5436ef7e1ab68a
Eric Ihli <eihli@owoga.com>
2025-04-04 00:38:49 -0700

    Possibly working zipper stepper

--- reason/racket/datagen-1.rkt
+++ reason/racket/datagen-1.rkt
@@ -11,112 +11,94 @@
 ;; --------------------------------------------------
 ;; Zipper Structure
 ;; --------------------------------------------------
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Define explore state to maintain the data held at every step
+;; when exploring
+;; https://wiki.haskell.org/Zipper
+(struct zip-ctx (index siblings parent) #:prefab)
+(struct zip-tre (s) #:prefab)
+(struct zip-loc (tree context) #:prefab)
+(define zip-TOP 'TOP)
+;; Initial zipper: ((zip-tre #f) (zip-ctx -1 '() zip-TOP))
 
-(struct loc (left path right focus) #:transparent)
+(define (flip i) (if (= i 0) 1 0))
 
-(define (make-initial-loc s)
-  (loc '() #f '() s))
+(define (mature? s)
+  (or (not s) (pair? s)))
+
+(define (mature z s)
+  (if (mature? s)
+      (cons z s)
+      (let ((result (step/zip z s)))
+        (mature (car result) (cdr result)))))
 
 ;; --------------------------------------------------
 ;; Zipper Utilities (for later)
 ;; --------------------------------------------------
-
-(define (loc-up z)
-  (match z
-    [(loc left (loc pl pp pr pf) right focus)
-     (loc (cons focus left) pp pr pf)]
-    [_ #f]))
-
-(define (loc-redo z)
-  (match z
-    [(loc left path (cons r rs) focus)
-     (loc (cons focus left) path rs r)]
-    [_ #f]))
-
-(define (loc-undo z)
-  (match z
-    [(loc (cons l ls) path right focus)
-     (loc ls path (cons focus right) l)]
-    [_ #f]))
-
-;; --------------------------------------------------
-;; Zipper-aware maturity
-;; --------------------------------------------------
-
-(define (mature? z)
-  (let ((s (loc-focus z)))
-    (or (not s) (pair? s))))
-
-(define (mature z)
-  (if (mature? z)
-      z
-      (mature (step/zipper z))))
-
-;; --------------------------------------------------
-;; Zipper-aware Step Function (Corrected)
-;; --------------------------------------------------
-
-
-(define (step/zipper z)
-  (let ((s (loc-focus z)))
-    (match s
-      ;; === mplus ===
-      [(mplus s1 s2)
-       (let ((s1 (if (mature? (make-initial-loc s1)) s1 (loc-focus (step/zipper (make-initial-loc s1))))))
+(define (step/zip z s)
+  (match s
+    ((mplus s1 s2)
+     (let* ((ctx (zip-loc-context z))
+            (cur-idx (zip-ctx-index ctx))
+            (nxt-idx (flip cur-idx)))
+       (let* ((s1 (if (= 0 cur-idx)
+                      (if (mature? s1) s1 (cdr (step/zip z s1)))
+                      s1))
+              (s2 (if (= 1 cur-idx)
+                      (if (mature? s2) s2 (cdr (step/zip z s2)))
+                      s2)))
          (cond
-           ((not s1)
-            (loc '() z '() s2)) ;; dead s1
-
-           ((pair? s1)
-            ;; ✅ NOTICE: returning a SINGLE loc
-            (loc '() z (list s2) (car s1))) ;; biased interleaving, take head
-
-           (else
-            (loc (list s2) z (list s1) (mplus s2 s1)))))]
-      ;; === bind ===
-      [(bind s g)
-       (let ((s (if (mature? (make-initial-loc s)) s (loc-focus (step/zipper (make-initial-loc s))))))
-         (cond
-           ((not s)
-            ;; <=== ✅ instead of returning #f, return a loc representing the dead-end
-            (loc '() z '() #f))
-           ((pair? s)
-            (step/zipper (loc (list (pause (car s) g))
-                              z
-                              (list (bind (cdr s) g))
-                              (mplus (pause (car s) g)
-                                     (bind (cdr s) g)))))
-           (else (loc '() z '() (bind s g)))))]
-
-      ;; === pause ===
-      [(pause st g)
-       (loc '() z '() (start st g))]
-
-      ;; === mature ===
-      [else z])))
-
+           ((not s1) (cons (zip-loc (zip-tre s2) (zip-ctx nxt-idx `(,s1) (zip-loc-context z))) s2))
+           ((not s2) (cons (zip-loc (zip-tre s1) (zip-ctx nxt-idx `(,s2) (zip-loc-context z))) s1))
+           ((pair? s1) (cons (zip-loc (zip-tre (cons (car s1) (mplus (cdr s1) s2)))
+                                      (zip-ctx cur-idx `(,s2) (zip-loc-context z)))
+                             (cons (car s1) (mplus (cdr s1) s2))))
+           ((pair? s2) (cons (zip-loc (zip-tre (cons (car s2) (mplus s1 (cdr s2))))
+                                      (zip-ctx cur-idx `(,s1) (zip-loc-context z)))
+                             (cons (car s2) (mplus s1 (cdr s2)))))
+           (else (cons (zip-loc (zip-tre (mplus s1 s2)) (zip-ctx nxt-idx `(,(if (= 0 nxt-idx) s2 s1)) (zip-loc-context z)))
+                       (mplus s1 s2)))))))
+    ((bind s g)
+     (let* ((res (if (mature? s) (cons z s)
+                     (step/zip z s)))
+            (nxt-z (car res))
+            (nxt-s (cdr res)))
+       (cond ((not nxt-s) (cons nxt-z #f))
+             ((pair? nxt-s)
+              (step/zip nxt-z (mplus (pause (car nxt-s) g)
+                                     (bind (cdr nxt-s) g))))
+             (else (cons nxt-z (bind nxt-s g))))))
+    ((pause st g)
+     (cons (zip-loc (zip-tre (start st g)) (zip-loc-context z))
+           (start st g)))
+    (_ (cons z s))))
 
 ;; --------------------------------------------------
 ;; Zipper-Aware Stream Take
 ;; --------------------------------------------------
-
-(define (stream-take/traced n z)
+(define (stream-take/zip n z s)
   (if (zero? n)
       '()
-      (let ((mz (mature z)))
-        (let ((res (loc-focus mz)))
-          (if (pair? res)
-              (cons mz (stream-take/traced (- n 1) (make-initial-loc (cdr res))))
+      (let ((result (mature z s)))
+        (let ((new-z (car result))
+              (new-s (cdr result)))
+          (if (pair? new-s)
+              (cons (cons new-z (car new-s))
+                    (stream-take/zip (and n (- n 1))
+                                    (zip-loc (zip-tre (cdr new-s))
+                                            (zip-ctx 0 '() (zip-loc-context new-z)))
+                                    (cdr new-s)))
               '())))))
+
 
 ;; --------------------------------------------------
 ;; API: run/traced
 ;; --------------------------------------------------
 
-(define-syntax run/traced
+(define-syntax run/zip
   (syntax-rules ()
     ((_ n body ...)
-     (stream-take/traced n (make-initial-loc (query body ...))))))
+     (stream-take/zip n (zip-loc (query body ...) (zip-ctx 0 '() zip-TOP)) (query body ...)))))
 
 (define-relation (appendo a b ab)
   (conde
@@ -126,35 +108,26 @@
            (== ab `(,a1 . ,res))
            (appendo a2 b res)))))
 
-(cadr (run/traced 5 (a b) (appendo a b '(1 2 3 4))))
-
-(map (lambda (x) (loc-right x)) (run/traced 10 (a b) (appendo a b '(1 2 3 4))))
-
-
-(define (path-chosen x)
-  (cond
-    ((false? x)    #f)
-    ((loc-right x) 0)
-    ((loc-left x)  1)
-    (else          '?)))
-
-(let ((solution (car (run/traced 10 (a b) (appendo a b '(1 2 3 4))))))
-  (let loop ((path '())
-             (solution solution))
-    (if (false? solution)
-        (map path-chosen (reverse path))
-        (loop (cons (loc-path solution) path)
-              (loc-path solution)))))
+;; Traverse up a zipper and collect the path of choices (0 for s1, 1 for s2)
+;; Returns a list of indices representing the path from root to current location
+(define (zipper->path z)
+  (let loop ([ctx (zip-loc-context z)]
+             [path '()])
+    (cond
+      [(eq? ctx zip-TOP) (reverse path)]  ; We've reached the top, reverse for root-to-leaf order
+      [else
+       (let ([idx (zip-ctx-index ctx)]
+             [parent (zip-ctx-parent ctx)])
+         (loop parent (cons idx path)))])))  ; Accumulate indices as we go up
 
 (comment
-(define foo (run/traced 1 (fn arg out) (eval-expo `(app ,fn ,arg) '() out)))
+
+ (zipper->path (caar (reverse (run/zip 10 (a b) (appendo a b '(1 2 3 4))))))
 
  )
 
-;; (let ((solution (car (run/traced 1 (fn arg out) (eval-expo `(app ,fn ,arg) '() out)))))
-;;   (let loop ((path '())
-;;              (solution solution))
-;;     (if (false? solution)
-;;         (map path-chosen (reverse path))
-;;         (loop (cons (loc-path solution) path)
-;;               (loc-path solution)))))
+(comment (cadr (run/zip 5 (a b) (appendo a b '(1 2 3 4)))))
+
+(comment
+
+ )
